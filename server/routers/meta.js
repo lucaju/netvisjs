a08499984ed6ee25bb3cbba3dc19e7cf11c5d370
Luciano Frizzera <lucaju@me.com>
2020-03-31 21:05:58 -0400

    style: 💄 Add inline docuemntation

--- server/routers/meta.js
+++ server/routers/meta.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-prototype-builtins */
 const express = require('express');
 
 const auth = require('../middleware/auth');
@@ -7,6 +6,17 @@ const Meta = require('../models/meta');
 const router = new express.Router();
 router.use(express.json());
 
+
+/**
+ * GET Meta Property
+ * 
+ * @async
+ * @function
+ * @param {String} req.params.prop Matches  meta[prop] : Use 'all' to show all
+ * @returns {Mixin} res.body - A meta Property or an array os properties
+ * @example
+ * get/title
+ */
 router.get('/:prop', async (req, res) => {
 	const meta = await Meta.findOne()
 		.catch( (error) => {
@@ -15,7 +25,9 @@ router.get('/:prop', async (req, res) => {
         });
 
     //if not all or prop doesn't exist
-    if (req.params.prop !== 'all' && meta[req.params.prop] === undefined) return res.status(400).send('Property not found');
+    if (req.params.prop !== 'all' && meta[req.params.prop] === undefined) {
+        return res.status(400).send('Property not found');
+    }
     
     if(req.params.prop !== 'all') {
         res.status(200).send(meta[req.params.prop]);
@@ -24,6 +36,21 @@ router.get('/:prop', async (req, res) => {
     }
 });
 
+/**
+ * PORT Meta Property
+ * 
+ * @async
+ * @function
+ * @param {Object} req.body Properties 
+ * @param {String} req.body.install flag installation process required to create properties on Meta document
+ * @returns {Object} res.body - Meta properties
+ * @example
+ * post/
+ * {
+ *  install: true,
+ *  title: 'title'
+ * }
+ */
 router.post('/', async (req, res) => {
     try {
         if (!req.body.install) throw new Error(); 
@@ -33,38 +60,55 @@ router.post('/', async (req, res) => {
         await meta.save();
         
         res.status(201).send(meta);
+
     } catch (error) {
         res.status(400).send(error);
 	}
 });
 
+/**
+ * PATCH Update Meta Properties
+ * 
+ * @async
+ * @function
+ * @param {Object} req.body Properties 
+ * @returns {Object} res.body - Meta properties
+ * @example
+ * patch/
+ * {
+ *  title: 'title'
+ * }
+ */
 router.patch('/', auth, async (req, res) => {
 	const updates = Object.keys(req.body);
 
 	try {
-        const meta = await Meta.findOne()
-            .catch( (error) => {
-                console.log(error);
-                res.status(400).send(error);
-            });
+        const meta = await Meta.findOne();
 
         updates.forEach((update) => meta[update] = req.body[update]);
         await meta.save();
 
-		res.send(meta);
+		res.status(200).send(meta);
 
 	} catch (error) {
 		res.status(400).send(error);
 	}
 });
 
+
+/**
+ * Delete Meta Property
+ * 
+ * @async
+ * @function
+ * @param {Object} req.params.prop Meta Property 
+ * @returns {Object} res.body - Meta properties
+ * @example
+ * delete/title
+ */
 router.delete('/:prop', auth, async (req, res) => {
 	try {
-        const meta = await Meta.findOne()
-            .catch( (error) => {
-                console.log(error);
-                res.status(400).send(error);
-            });
+        const meta = await Meta.findOne();
         
         //remove property;
         const newMeta =  meta.toObject();
@@ -73,7 +117,7 @@ router.delete('/:prop', auth, async (req, res) => {
         await meta.overwrite(newMeta);
         await meta.save();
 
-		res.send(meta);
+		res.status(200).send(meta);
 
 	} catch (error) {
 		res.status(400).send(error);
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const {DateTime} = require ('luxon');
-const mongoose = require('mongoose');
 
 const auth = require('../middleware/auth');
 const User = require('../models/user');
@@ -9,46 +8,104 @@ const User = require('../models/user');
 const router = new express.Router();
 router.use(express.json());
 
-router.get('/', auth, async (req, res) => {
-	const users = await User.find().sort({firstName: 'asc'})
-		.catch( (error) => {
-			console.log(error);
-		});
-	res.send(users);
-});
 
+/**
+ * GET User by ID
+ * 
+ * @async
+ * @function
+ * @param {String} req.params.id Matches the user._id : 'me' refers to current user
+ * @returns {Object} res.body - User
+ * @example
+ * get/:_id
+ */
 router.get('/:id', auth, async (req, res) => {
-
 	//if cuurrent user
 	if (req.params.id === 'me') res.send(req.user);
 
 	//any other user
-	const userID = mongoose.Types.ObjectId(req.params.id);
-	const user = await User.findById(userID)
-		.catch( (error) => {
-			console.log(error);
+	const user = await User.findById(req.params.id)
+		.catch(error => {
+			res.status(500).send(error);
 		});
-	res.send(user);
+
+	if (!user) res.status(404).send();
+
+	res.status(200).send(user);
 });
 
+/**
+ * GET Users
+ * 
+ * @async
+ * @function
+ * @returns {Object} res.body - Users Collection
+ * @example
+ * get/
+ */
+router.get('/', auth, async (req, res) => {
+	const users = await User.find().sort({firstName: 'asc'})
+		.catch( (error) => {
+			res.status(500).send(error);
+		});
+	
+	if (!users) res.status(404).send();
+
+	res.status(200).send(users);
+});
+
+/**
+ * POST User
+ * 
+ * @async
+ * @function
+ * @param {Object} req.body User data
+ * @returns {Object} res.body - User
+ * @example
+ * post/
+ * {
+ * 	name: name,
+ * 	email: email,
+ * ...
+ * }
+ */
 router.post('/', async (req, res) => {
-	const user = new User(req.body);
     try {
+		const user = new User(req.body);
 		await user.save();
+
+		const token = await user.generateAuthToken();
 		// sendWelcomeEmail(user.email, user.name)
-        const token = await user.generateAuthToken();
-        res.status(201).send({ user, token });
+        
+        res.status(201).send({user, token});
     } catch (error) {
         res.status(400).send(error);
 	}
 });
 
+/**
+ * PATCH Update User
+ * 
+ * @async
+ * @function
+ * @param {String} req.params.id User id
+ * @param {Object} req.body User data
+ * @returns {Object} res.body - User
+ * @example
+ * patch/
+ * {
+ * 	name: name,
+ * 	email: email,
+ * ...
+ * }
+ */
 router.patch('/:id', auth, async (req, res) => {
 
+	//check valid operation
 	const updates = Object.keys(req.body);
 	const allowedUpdates = ['firstName', 'lastName', 'email', 'password', 'level'];
 	const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
+	
 	const passwordUpdate = allowedUpdates.find( type => type === 'password');
 
 	if (!isValidOperation) {
@@ -58,118 +115,184 @@ router.patch('/:id', auth, async (req, res) => {
 	try {
 		let user;
 		if (req.params.id === 'me') {
-			//if cuurrent user
-			user = req.user;
+			user = req.user; //if cuurrent user
 		} else {
-			const userID = mongoose.Types.ObjectId(req.params.id);
-			user = await User.findById(userID);
+			user = await User.findById(req.params.id);
 		}
 
 		updates.forEach((update) => user[update] = req.body[update]);
 		if (passwordUpdate) user.tokens = [];
 
 		await user.save();
-		res.send(user);
+		res.status(200).send(user);
 
 	} catch (error) {
 		res.status(400).send(error);
 	}
 });
 
+/**
+ * DELETE User
+ * 
+ * @async
+ * @function
+ * @param {String} req.params.id User id
+ * @returns {Object} res.body - User
+ * @example
+ * delete/:id
+ */
 router.delete('/:id', auth, async (req, res) => {
-
 	try {
 		let user;
 		if (req.params.id === 'me') {
-			//if cuurrent user
-			user = req.user;
+			user = req.user; //if cuurrent user
 		} else {
-			const userID = mongoose.Types.ObjectId(req.params.id);
-			user = await User.findById(userID);
+			user = await User.findById(req.params.id);
 		}
 
 		await User.deleteOne({_id: user.id});
 		// sendCancelationEmail(req.user.email, req.user.name)
 
-		res.send(user);
+		res.status(200).send(user);
 
 	} catch (e) {
 		res.status(500).send();
 	}
-
 });
 
+/**
+ * POST Login
+ * 
+ * @async
+ * @function
+ * @param {String} req.body.email User email
+ * @param {String} req.body.password User password
+ * @returns {Object} res.body - {User, token}
+ * @example
+ * post/login
+ * {
+ * 	email: email,
+ * 	password: password
+ * }
+ */
 router.post('/login', async (req, res) => {
 	try {
 		const user = await User.findByCredentials(req.body.email, req.body.password);
-		console.log(User);
         const token = await user.generateAuthToken();
-        res.send({ user, token });
+        res.status(200).send({user, token});
     } catch (e) {
         res.status(400).send();
     }
 });
 
+/**
+ * POST Logout
+ * 
+ * @async
+ * @function
+ * @example
+ * post/logout
+ */
 router.post('/logout', auth, async (req, res) => {
+	//remove current token
 	req.user.tokens = req.user.tokens.filter((token) => {
 		return token.token !== req.token;
 	});
 
 	await req.user.save()
-		.catch(() => res.status(500).send());
+		.catch(() => {
+			res.status(500).send();
+		});
 
-	res.send();
+	res.status(200).send();
 });
 
+/**
+ * POST Logout from everywhere
+ * 
+ * @async
+ * @function
+ * @example
+ * post/logoutAll
+ */
 router.post('/logoutAll', auth, async (req, res) => {
+	//reset tokens
 	req.user.tokens = [];
 
 	await req.user.save()
-		.catch(() => res.status(500).send());
+		.catch(() => {
+			res.status(500).send();
+		});
 
-	res.send();
+	res.status(200).send();
 });
 
+/**
+ * POST Forgot Password
+ * 
+ * @async
+ * @function
+ * @param {String} req.body.email User email
+ * @example
+ * post/forgotPassword
+ */
 router.post('/forgotPassword', async (req, res) => {
-	const userEmail = req.body.email;
-
-	const user = await User.findByEmail(userEmail)
-		.catch((error) => res.status(404).send(error));
+	//find user
+	const user = await User.findByEmail(req.body.email)
+		.catch((error) => {
+			res.status(404).send(error);
+		});
 
 	const token = await user.generatePwdToken();
 	// sendResetPasswordEmail(user.id, user.email, token)
+
 	res.status(200).send();
 
 });
 
+/**
+ * POST Reset Password
+ * 
+ * @async
+ * @function
+ * @param {String} req.params.id User id
+ * @param {String} req.params.token User token
+ * @returns {Object} res.body - User
+ * @example
+ * post/resetPassword/:id/:token
+ */
 router.get('/resetPassword/:id/:token', async (req, res) => {
-	const userID = mongoose.Types.ObjectId(req.params.id);
-	const passToken = req.params.token;
-
 	//find user
-	const user = await User.findById(userID)
-		.catch((error) => res.status(404).send(error));
+	const user = await User.findById(req.params.id)
+		.catch((error) => {
+			res.status(404).send(error);
+		});
 
 	//find token
 	const passwordReset = user.passwordResetTokens.find( pr => {
-		if (pr.token === passToken && pr.used === false) return pr;
+		if (pr.token === req.params.token && pr.used === false) return pr;
 	});
 
 	//Token not found or already used
-	if (passwordReset === undefined) return res.status(404).send('Token expired or not found');
+	if (passwordReset === undefined) {
+		return res.status(404).send('Token expired or not found');
+	}
 
 	//Token expires in X days
 	const now = DateTime.utc();
 	const tokenDate = DateTime.fromJSDate(passwordReset.date);
 	const diffInDays = now.diff(tokenDate, 'days');
 
-	if (diffInDays.toObject().days > 5) return res.status(406).send('Token expired');
+	//if tokens has more than 5 days
+	if (diffInDays.toObject().days > 5) {
+		return res.status(400).send('Token expired');
+	}
 
 	//ok, mark token as used
-	await user.usePwdToken(passToken);
+	await user.usePwdToken(req.params.token);
 
-	//pass user indo to the interface
-	res.status(200).send({user});
+	//pass user info to the interface
+	res.status(200).send(user);
 });
 
 module.exports = router;
