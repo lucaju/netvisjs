c07f6e9992bc9ac34de89ac941d7e088f1ae8caa
Luciano Frizzera <lucaju@me.com>
2020-03-31 21:04:53 -0400

    style: 💄 organize statetments

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
+
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, 'netvis');
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+        const user = await User.findOne({
+            _id: decoded._id,
+            'tokens.token': token
+        });
         
         if (!user) throw new Error();
 
@@ -13,7 +17,7 @@ const auth = async (req, res, next) => {
         req.user = user;
         next();
     } catch (error) {
-        res.status(401).send({ error: 'Please authenticate.' });
+        res.status(401).send({ error: 'Please Authenticate.' });
     }
 };
 
