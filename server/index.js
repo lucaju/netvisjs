9bd0a87f787e1414faede1deeb3d988c4066ada5
Luciano Frizzera <lucaju@me.com>
2020-04-07 23:38:01 -0400

    feat: 🎸 add routers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoDB = require('./db/mongoDB');
 const meta = require('./routers/meta');
 const user = require('./routers/user');
 const node = require('./routers/node');
+const reset = require('./routers/reset');
 
 const app = express();
 
@@ -12,13 +13,13 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // support json encoded bodies
 
-app.use(express.static('dist'));
-app.use('/assets', express.static(__dirname + '/dist/assets'));
-
 //routers
-app.use('/meta',meta);
-app.use('/users',user);
-app.use('/nodes',node);
+app.use('/meta', meta);
+app.use('/users', user);
+app.use('/nodes', node);
+app.use('/reset', reset);
+
+app.use('/', express.static('dist'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -29,11 +30,11 @@ app.use((req, res, next) => {
 
 app.listen(port, async () => {
 
-	//commect MongoDB
-	// await mongoDB.connect()
-	// 	.catch( error => {
-	// 		console.log(error);
-	// 	});
+	// connect MongoDB
+	await mongoDB.connect()
+		.catch(() => {
+			console.log('NetVis need to be install!');
+		});
 
 	console.log(`App listening on port ${port}!`);
 });
