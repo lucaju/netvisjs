28883274bb4c18f7bc997f5282edd9e99a950673
Luciano Frizzera <lucaju@me.com>
2020-04-11 20:35:22 -0400

    refactor: 💡 Splip app and server

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const meta = require('./routers/meta');
+const user = require('./routers/user');
+const node = require('./routers/node');
+const reset = require('./routers/reset');
+
+const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+
+//routers
+app.use('/meta', meta);
+app.use('/users', user);
+app.use('/nodes', node);
+app.use('/reset', reset);
+
+app.use('/', express.static('dist'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not  Found');
+	err.status = 404;
+	next(err);
+});
+
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,32 +1,7 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-
+const app = require('./app');
 const mongoDB = require('./db/mongoDB');
-const meta = require('./routers/meta');
-const user = require('./routers/user');
-const node = require('./routers/node');
-const reset = require('./routers/reset');
-
-const app = express();
-
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json()); // support json encoded bodies
-
-//routers
-app.use('/meta', meta);
-app.use('/users', user);
-app.use('/nodes', node);
-app.use('/reset', reset);
-
-app.use('/', express.static('dist'));
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-	const err = new Error('Not  Found');
-	err.status = 404;
-	next(err);
-});
 
 app.listen(port, async () => {
 
