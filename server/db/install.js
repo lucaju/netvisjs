ac17c155fc87625f19fd49efbdce3108c502e603
Luciano Frizzera <lucaju@me.com>
2024-08-24 22:06:01 -0400

    fix mongo url

--- server/db/install.js
+++ server/db/install.js
@@ -20,7 +20,7 @@ const install = async () => {
     const mongoDB = `/${process.env.MONGODB_DATABASE}`;
 
     // process.env.MONGO_URI = `mongodb://${mongoCredentials}${mongoServer}${mongoDB}?authSource=admin`;
-    process.env.MONGO_URI = `mongodb://${mongoCredentials}@142.132.164.59:${process.env.MONGODB_PORT}/${mongoDB}?authSource=admin`
+    process.env.MONGO_URI = `mongodb://${process.env.MONGODB_ROOT_USERNAME}:${process.env.MONGODB_ROOT_PASSWORD}@142.132.164.59:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}?authSource=admin`
     console.log(process.env.MONGO_URI)
 
     //C. Connect MongoDB
