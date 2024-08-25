30ef7d6950fb2b4deadc89a2e5ecbf7c44a7a395
Luciano Frizzera <lucaju@me.com>
2024-08-24 21:52:01 -0400

    user env vars

--- server/db/install.js
+++ server/db/install.js
@@ -19,8 +19,9 @@ const install = async () => {
     const mongoServer = `${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
     const mongoDB = `/${process.env.MONGODB_DATABASE}`;
 
-    process.env.MONGO_URI = `mongodb://${mongoCredentials}${mongoServer}${mongoDB}?authSource=admin`;
-
+    // process.env.MONGO_URI = `mongodb://${mongoCredentials}${mongoServer}${mongoDB}?authSource=admin`;
+    process.env.MONGO_URI `mongodb://${mongoCredentials}@142.132.164.59:${process.env.MONGODB_PORT}/${mongoDB}?authSource=admin`
+    console.log(process.env.MONGO_URI)
 
     //C. Connect MongoDB
     let connectionCoolDown = 3000;
